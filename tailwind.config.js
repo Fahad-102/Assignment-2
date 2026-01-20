4ec552d70427d74064a3618ed24bf616183b87f8
Fahad-102 <muhammadfahadbinjamal@gmail.com>
2026-01-20 23:18:56 +0600

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '576px',   // small device start
+      'md': '576px',   // medium device start
+      'lg': '993px',   // large device start
+    },
+  },
+  plugins: [],
+}
